ab84e942802a083dd806f6a018dcc59c4bc007de
Juil Park <ju4756@gmail.com>
2023-09-11 14:35:56 +0900

    ~ 3-12 앵커

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponseNotAllowed
+from django.shortcuts import render, get_object_or_404, redirect, resolve_url
 from django.utils import timezone
 
 from ..forms import AnswerForm
@@ -11,7 +12,8 @@ def answer_modify(request, answer_id):
     answer = get_object_or_404(Answer, pk=answer_id)
     if request.user != answer.author:
         messages.error(request, '수정권한이 없습니다.')
-        return redirect('pybo:detail', question_id=answer.question.id)
+        return redirect('{}#answer_{}'.format(
+            resolve_url('pybo:detail', question_id=answer.question.id), answer.id))
     if request.method == "POST":
         form = AnswerForm(request.POST, instance=answer)
         if form.is_valid():
@@ -45,7 +47,8 @@ def answer_create(request, question_id):
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
-            return redirect('pybo:detail', question_id=question.id)
+            return redirect('{}#answer_{}'.format(
+                resolve_url('pybo:detail', question_id=answer.question.id), answer.id))
     else:
         return HttpResponseNotAllowed('Only POST is possible.')
     context = {'question': question, 'form': form}
@@ -58,4 +61,5 @@ def answer_vote(request, answer_id):
         messages.error(request, '본인이 작성한 글은 추천할수 없습니다')
     else:
         answer.voter.add(request.user)
-    return redirect('pybo:detail', question_id=answer.question.id)
+    return redirect('{}#answer_{}'.format(
+        resolve_url('pybo:detail', question_id=answer.question.id), answer.id))
